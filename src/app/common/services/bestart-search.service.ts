5d4f870a26e611a01394fd73633f764215fbe627
Marina <marina.dobrogorskaya@gmail.com>
2018-08-10 22:07:19 +0300

    Refactoring of small issues

--- src/app/common/services/bestart-search.service.ts
+++ src/app/common/services/bestart-search.service.ts
@@ -9,17 +9,17 @@ import {API_KEY, BA_ULR} from '../../constants';
   providedIn: 'root'
 })
 export class BestArtSearchService {
-  public url = BA_ULR;
+  private url = BA_ULR;
   constructor(private http: HttpClient) { }
   getBestArticles(category, section, period): Observable<BestArticle[]> {
+    const _url = this.url + category + '/' + section + '/' + period + '.json';
     const imgNum = category === 'mostviewed' && section === 'all-sections' ? 3 : 2;
     const options = {
       params: new HttpParams()
         .set('api-key', API_KEY)
     };
-    this.url += category + '/' + section + '/' + period + '.json';
-    console.log(this.url);
-    return this.http.get<any>(this.url, options).pipe(map(obj => {
+    console.log(_url);
+    return this.http.get<any>(_url, options).pipe(map(obj => {
       const articles = obj['results'];
       return articles.map(article => {
         const img = article.media[0]['media-metadata'].length > 2 ? article.media[0]['media-metadata'][imgNum].url : undefined;
