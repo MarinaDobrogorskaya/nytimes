def321df76711f5e8534d32478d2946ee96c8502
marina <marina.dobrogorskaya@gmail.com>
2018-08-13 18:31:32 +0300

    chops and changes

--- src/app/content/articles/search/search.component.ts
+++ src/app/content/articles/search/search.component.ts
@@ -1,7 +1,6 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {AdvancedSearch, Search} from './search-events';
-import {Renderer2} from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -10,30 +9,29 @@ import {Renderer2} from '@angular/core';
 })
 export class SearchComponent implements OnInit {
   private TAG = 'SearchComponent >';
-  private input = document.getElementsByTagName('input');
   public query = new FormControl('');
   public isOpened = false;
   private searchData: Search = {
     query: ''
   };
   @Output() search = new EventEmitter<Search>();
-  constructor(private renderer: Renderer2) { }
+  constructor() { }
   toggle(): void {
     this.isOpened ? this.isOpened = false : this.isOpened = true;
   }
   ngOnInit() {
-    this.renderer.listen(this.input[0], 'keydown', (event) => {
-      if (event.keyCode === 13) {
-        this.onSearch();
-      }
-    });
   }
-  onChange(data: AdvancedSearch) {
+  onChange(data: AdvancedSearch): void {
     Object.assign<Search, AdvancedSearch>(this.searchData, data as AdvancedSearch);
   }
-  onSearch() {
+  onSearch(): void {
     this.searchData.query = this.query.value;
     console.log(`${this.TAG} onSearch: this.searchData - ${this.searchData}`);
     this.search.emit(this.searchData);
+  }
+  onEnterPressed(event: KeyboardEvent): void {
+    if (event.keyCode === 13) {
+      this.onSearch();
+    }
   }
 }
